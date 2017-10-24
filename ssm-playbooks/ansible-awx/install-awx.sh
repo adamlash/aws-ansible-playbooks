e94a21a49004a4d1e4c59babca4acb3e7f4dcfa7
Adam Lash <adam.lashh@gmail.com>
2017-10-24 17:04:03 -0400

    adding in a line to edit the install locale of postgres out of the tmp dir

--- ssm-playbooks/ansible-awx/install-awx.sh
+++ ssm-playbooks/ansible-awx/install-awx.sh
@@ -4,4 +4,6 @@ cd /tmp
 git clone https://github.com/ansible/awx.git
 # Jump into the ansible install dir and run the installer.
 cd awx/installer
+# change the postgres install location, so it doesn't get wiped on boot.
+sed -i 's/postgres_data_dir=\/tmp\/pgdocker/postgres_data_dir=\/data\/pgdocker/' testinventory
 ansible-playbook -i inventory install.yml
